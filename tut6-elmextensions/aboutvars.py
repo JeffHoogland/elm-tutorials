bb4dd1265b75282fc527649475b2dbc049ff897b
Jeff Hoogland <jeffhoogland@linux.com>
2015-11-01 15:36:43 -0600

    Added source for sixth tutorial

---
+++ tut6-elmextensions/aboutvars.py
@@ -0,0 +1,38 @@
+AUTHORS = """
+<br>
+<align=center>
+<hilight>Jeff Hoogland (Jef91)</hilight><br>
+<link><a href=http://www.jeffhoogland.com>Contact</a></link><br><br>
+</align>
+"""
+
+LICENSE = """
+<align=center>
+<hilight>
+GNU GENERAL PUBLIC LICENSE<br>
+Version 3, 29 June 2007<br><br>
+</hilight>
+
+This program is free software: you can redistribute it and/or modify 
+it under the terms of the GNU General Public License as published by 
+the Free Software Foundation, either version 3 of the License, or 
+(at your option) any later version.<br><br>
+
+This program is distributed in the hope that it will be useful, 
+but WITHOUT ANY WARRANTY; without even the implied warranty of 
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the 
+GNU General Public License for more details.<br><br>
+
+You should have received a copy of the GNU General Public License 
+along with this program. If not, see<br>
+<link><a href=http://www.gnu.org/licenses>http://www.gnu.org/licenses/</a></link>
+</align>
+"""
+
+INFO = """
+<align=center>
+<hilight>Elementary Python Extensions</hilight> are awesome!<br> 
+<br>
+<br>
+</align>
+"""
---
+++ tut6-elmextensions/ex8-elmex.py
@@ -0,0 +1,57 @@
+import efl.elementary as elm
+from efl.elementary.window import StandardWindow
+from efl.elementary.box import Box
+
+from elmextensions import StandardButton
+from elmextensions import StandardPopup
+from elmextensions import AboutWindow
+
+from aboutvars import *
+
+from efl.evas import EVAS_HINT_EXPAND, EVAS_HINT_FILL
+EXPAND_BOTH = EVAS_HINT_EXPAND, EVAS_HINT_EXPAND
+EXPAND_HORIZ = EVAS_HINT_EXPAND, 0.0
+FILL_BOTH = EVAS_HINT_FILL, EVAS_HINT_FILL
+
+class MainWindow(StandardWindow):
+    def __init__(self):
+        StandardWindow.__init__(self, "ex8", "ElmEx - Button and Popup", size=(300, 200))
+        self.callback_delete_request_add(lambda o: elm.exit())
+
+        ourButton = StandardButton(self, "Show Popup", "start-here", self.buttonPressed, size_hint_weight = EXPAND_HORIZ)
+        ourButton.size_hint_weight = EXPAND_HORIZ
+        ourButton.size_hint_align = FILL_BOTH
+        ourButton.show()
+        
+        ourButton2 = StandardButton(self, "Show About", "dialog-information", self.button2Pressed)
+        ourButton2.size_hint_weight = EXPAND_HORIZ
+        ourButton2.size_hint_align = FILL_BOTH
+        ourButton2.show()
+        
+        self.ourPopup = StandardPopup(self, "Press OK to close this message.", "ok")
+        
+        mainBox = Box(self)
+        mainBox.size_hint_weight = EXPAND_BOTH
+        mainBox.size_hint_align = FILL_BOTH
+        mainBox.pack_end(ourButton)
+        mainBox.pack_end(ourButton2)
+        mainBox.show()
+        
+        self.resize_object_add(mainBox)
+        
+    def buttonPressed(self, btn):
+        self.ourPopup.show()
+    
+    def button2Pressed(self, btn):
+        AboutWindow(self, title="About Window", standardicon="dialog-information", \
+                        version="1.0", authors=AUTHORS, \
+                        licen=LICENSE, webaddress="https://github.com/JeffHoogland/python-elm-extensions", \
+                        info=INFO)
+
+if __name__ == "__main__":
+    elm.init()
+    GUI = MainWindow()
+    GUI.show()
+    elm.run()
+    elm.shutdown()
+
